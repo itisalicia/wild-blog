0efe358230ad71df97e06a7dfa60715226352a7b
Alicia <alicia.magro@ouidou.fr>
2025-08-13 11:40:42 +0200

    Create a signup form with Reactive Forms, add validations and groupForm for password and confirm password

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomePageComponent } from './pages/home-page-component/home-page-compone
 import { ArticlePageComponent } from './pages/article-page-component/article-page-component';
 import { NotFoundPageComponent } from './pages/not-found-page-component/not-found-page-component';
 import { ContactPageComponent } from './pages/contact-page-component/contact-page-component';
+import { SignupForm } from './components/signup-form/signup-form';
 
 export const routes: Routes = [
     { path: '', pathMatch:'full', component: HomePageComponent },
     { path: 'article/:id', component: ArticlePageComponent },
     { path: 'contact', component: ContactPageComponent },
+    { path: 'signup', component: SignupForm },
 
     { path: '**', component: NotFoundPageComponent }
 ];
---
+++ src/app/components/signup-form/signup-form.ts
@@ -0,0 +1,72 @@
+import { Component, inject } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-signup-form',
+  imports: [ReactiveFormsModule],
+  templateUrl: './signup-form.html',
+  styleUrl: './signup-form.css',
+})
+export class SignupForm {
+  formBuilder = inject(FormBuilder);
+
+  signUpForm = this.formBuilder.group({
+    username: ['', [Validators.required, Validators.minLength(3)]],
+    email: ['', [Validators.required, Validators.email]],
+    passwords: this.formBuilder.group(
+      {
+        password: ['', [Validators.required, this.securePasswordValidator()]],
+        confirmPassword: [''],
+      },
+      { validators: this.passwordMatchValidator() }
+    ),
+  });
+
+  onSubmit() {
+    if (this.signUpForm.valid) {
+      console.log('Form submitted successfully!', this.signUpForm.value);
+    } else {
+      console.log('Form is invalid. Please check the fields.');
+    }
+  }
+
+  passwordMatchValidator(): ValidatorFn {
+    return (FormGroup: AbstractControl): ValidationErrors | null => {
+      const password = FormGroup.get('password')?.value;
+      const confirmPassword = FormGroup.get('confirmPassword')?.value;
+
+      return password === confirmPassword
+        ? null
+        : { passwordMatchValidator: true };
+    };
+  }
+
+  securePasswordValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const value = control.value || '';
+
+      const hasUpperCase = /[A-Z]/.test(value);
+      const hasLowerCase = /[a-z]/.test(value);
+      const hasNumber = /\d/.test(value);
+      const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(value);
+      const isValidLength = value.length >= 12;
+
+      const passwordValid =
+        hasUpperCase &&
+        hasLowerCase &&
+        hasNumber &&
+        hasSpecialChar &&
+        isValidLength;
+
+      return passwordValid ? null : { securePassword: true };
+    };
+  }
+}
